213d3d2ddec44275bcc1fb740f72482419f12cef
김영진 <dudwls0918@naver.com>
2026-07-06 21:26:22 +0900

    1
    1

--- be/src/main/java/com/example/boardbackend/domain/post/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.boardbackend.domain.post;
-
-//JpaRepository를 쓰기 위함 -> Spring Data Jpa가 제공하는 기본 DB기능 모음
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
